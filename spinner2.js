afb0be0c15e40c1299df757fa91031971157b393
Kevin Tan Yulo <yulokevin@gmail.com>
2021-11-29 18:50:22 -0800

    spinner 2

--- spinner2.js
+++ spinner2.js
@@ -1,8 +1,17 @@
-process.stdout.write('hello from spinner1.js... \rheyyy\n');
+let time = 50;
+let zero = 0;
 
+let printout = ["\r|", "\r/", "\r-", "\r\\", "\r|", "\r/", "\r-", "\r\\"]
 
+while (time < 3000) {
 
+  setTimeout(() => {
+    process.stdout.write(printout[zero]);
 
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 100);
+    zero = (zero + 1) % 7;
+
+  }, time);
+
+  time += 300;
+
+}
